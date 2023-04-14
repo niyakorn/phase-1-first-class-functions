f2b356a19a2ba8bf7de07856b174b7ab87fcc9c7
Taya Niyakorn <tayawee@gmail.com>
2023-04-14 23:31:27 +0700

    DONE

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const  returnFirstTwoDrivers = function anonymousFunction([]){
-    return (['Antonia', 'Nuru']);
-    
+function receivesAFunction(callback) {
+    return callback();
+}
+
+function returnsANamedFunction() {
+    return function receivesAFunction(name) {
+        console.log(name);
     }
-    
-    const  returnLastTwoDrivers = function anonymousFunction([]){
-        return (['Amari', 'Mo']);
-    
+}
+  
+function returnsAnAnonymousFunction() {
+    return function (name) {
+        console.log(name);
     }
-    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
-    
-    function createFareMultiplier(anInteger){
-        return function(fare){
-          return fare*anInteger;
-        };
-    }
-    const fareDoubler = createFareMultiplier(2);
-    const fareTripler = createFareMultiplier(3);
-    
-    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
-        return driversToReturn(arrayOfDrivers);
-      };
+}
